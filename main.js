94ebebc96a7fdef82cdc4450ac62ee4a71cca7cb
Guilherme Araujo <guidrop17@gmail.com>
2021-10-24 19:16:40 -0300

    upload files

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'guidrop17',
+  twitch: 'guiarauj00',
+  facebook: 'guilherme.araujo.58910',
+  instagram: 'guiaraujoac',
+  twitter: 'GuiArauj0o'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
